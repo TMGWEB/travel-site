434390818cd3ef465996d71988ea4d8c31d37a8e
TMGWEB <tmgriffiths1993@gmail.com>
2018-03-10 12:24:22 -0500

    Completed sticky header functionality.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
@@ -9,6 +10,11 @@ class StickyHeader {
     this.pageSections = $('.page-section');
     this.headerLinks = $('.primary-nav a');
     this.createPageSectionWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll();
   }
 
   createHeaderWaypoint() {
@@ -31,12 +37,26 @@ class StickyHeader {
       var currentPageSection = this;
       new Waypoint({
         element: currentPageSection,
-        handler: function() {
-          var matchingHeaderLink = currentPageSection.getAttribute('data-matching-link');
+        handler: function(direction) {
+          if (direction == "down") {
+            var matchingHeaderLink = currentPageSection.getAttribute('data-matching-link');
           that.headerLinks.removeClass('is-current-link');
           $(matchingHeaderLink).addClass('is-current-link');
+          }
         },
         offset: '18%'
+      });
+
+      new Waypoint({
+        element: currentPageSection,
+        handler: function(direction) {
+          if (direction == "up") {
+            var matchingHeaderLink = currentPageSection.getAttribute('data-matching-link');
+          that.headerLinks.removeClass('is-current-link');
+          $(matchingHeaderLink).addClass('is-current-link');
+          }
+        },
+        offset: '-40%'
       });
     });
   }
